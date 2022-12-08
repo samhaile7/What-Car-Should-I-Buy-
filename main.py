693db7cad76cff7e1715d98df840f0d6702d7588
Samuel Haile <samhaile129@gmail.com>
2022-12-07 22:14:41 -0500

    "brainstorming ideas"

---
+++ main.py
@@ -0,0 +1,10 @@
+# Brainstorm
+# Import csv of car makes and models
+#Iterate through each car and add door number, price ($, $$, $$$,$$$$$),
+# reliable or not (country manufactured), fun or not, asterisk on Sam's favorite
+# User input: ask how many doors needed, fun or not, etc. budget can be $ 0- 10k, $$ 10-20 etc.
+# Instruct user to use this as a guide to arrange test drives as those are important
+# links to dealer websites to schedule test drives
+# userinput intial: Welcome to what car should I buy. Enter your name:
+# store names in userlist csv. if name already exists: "your last results were ______, would you like to run a new search? y/n"
+
